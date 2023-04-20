9a5a168a5f3de6351652751e8b35115138802e7d
rubenarocha14 <rubenarocha14@gmail.com>
2023-04-20 10:51:03 -0600

    fix: format

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,13 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
-#Add x and y and reutrn result
 
+def add(x, y):
+    # Add x and y and return result
     return x + y
 
 
-def subtract(x,y):
-    #subtract x from y and return result
-
+def subtract(x, y):
+    # Subtract x from y and return result
     return y - x
--- app/app/test.py
+++ app/app/test.py
@@ -3,21 +3,18 @@ Sample tests
 """
 
 from django.test import SimpleTestCase
-
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    #Test Calc module
+    # Test Calc module
 
     def test_add_numbers(self):
-        #Test adding numbers together
-        res = calc.add(5,6)
-
-        self.assertEqual(res,11)
+        # Test adding numbers together
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        #subtracting numbers test
-        res = calc.subtract(10,15)
-
+        # subtracting numbers test
+        res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
-        
