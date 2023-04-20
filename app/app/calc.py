867126da79eeaa59564283e52bc0a85078e3d847
rubenarocha14 <rubenarocha14@gmail.com>
2023-04-20 10:38:30 -0600

    fix: setup django tests and db config

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+#Add x and y and reutrn result
+
+    return x + y
+
+
+def subtract(x,y):
+    #subtract x from y and return result
+
+    return y - x
---
+++ app/app/test.py
@@ -0,0 +1,23 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    #Test Calc module
+
+    def test_add_numbers(self):
+        #Test adding numbers together
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        #subtracting numbers test
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res, 5)
+        
