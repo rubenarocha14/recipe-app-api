d196d88b73e38c36985de9a7d842091c3187ba06
rubenarocha14 <rubenarocha14@gmail.com>
2023-04-24 13:23:12 -0600

    fix: Added user Models and normalization

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,7 +14,8 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
+                print(self.check(databases=['default']))
+                # self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
