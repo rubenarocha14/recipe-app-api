b1a337678906ca4d36cb7768052ab5bccd1e9bcc
rubenarocha14 <rubenarocha14@gmail.com>
2023-04-25 08:47:43 -0600

    fix: trying to fix wait_for_db error

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -31,3 +31,21 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email,'sample123')
             self.assertEqual(user.email, expected)
+
+
+    def test_new_user_without_email_raises_error(self):
+        #Test a new user w/o email raises Value Error
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123' )
+
+
+    def test_create_superuser(self):
+        #Test creating a super user
+
+        user = get_user_model().objects.create_superuser(
+            'test@example.com', 'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
